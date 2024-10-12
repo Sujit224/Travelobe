eca2964cd5ceae11159237f31853c8056f32555f
ThunderBlaze <darkthunderstormshiva@gmail.com>
2024-10-12 11:29:14 +0530

    Made the Website Responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 const hero = document.getElementById('hero');
+const header = document.getElementById('header');
 let a = "";
 
 function herobg(x) {
@@ -6,4 +7,8 @@ function herobg(x) {
 	a = a.slice(0,-17) + "1500&quality=80";
 	console.log(a);
 	hero.style.backgroundImage = a;
+}
+
+function ToggleHeader(x) {
+	header.classList.toggle("active");
 }
